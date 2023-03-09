c1d497bc19ff96b821ea48705958c99510bfb9dc
ykushnir <ykushnir@tripadvisor.com>
2023-03-09 19:47:49 +0200

    cosmetic changes

--- terraform/ha_dr_mon/launch_template.tf
+++ terraform/ha_dr_mon/launch_template.tf
@@ -1,17 +1,3 @@
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-module "aws-networking" {
-  source                   = "../modules/roadtomiddle_aws_networking"
-  common_name              = "test"
-  vpc_cidr                 = "192.168.0.0/16"
-  public_subnet_cidr_list  = ["192.168.1.0/24", "192.168.3.0/24"]
-  private_subnet_cidr_list = ["192.168.2.0/24", "192.168.4.0/24"]
-}
-
 resource "aws_launch_template" "launch_template" {
   depends_on    = [module.aws-networking]
   name          = "${var.common_name}-launch_template"
